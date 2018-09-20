aa46669d69b7b24d87d091d533d5a2f15a195267
StankovicMarko89 <43344688+StankovicMarko89@users.noreply.github.com>
2018-09-20 14:11:26 +0200

    Lägger till två knappar i receptionist form, samt sätter en minimum storlek på formen.

--- TandVerk/TandVerk/Receptionist.Designer.cs
+++ TandVerk/TandVerk/Receptionist.Designer.cs
@@ -28,11 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.läggTillPatient = new System.Windows.Forms.Button();
+            this.ändraBokning = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // läggTillPatient
+            // 
+            this.läggTillPatient.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.läggTillPatient.Location = new System.Drawing.Point(31, 68);
+            this.läggTillPatient.Name = "läggTillPatient";
+            this.läggTillPatient.Size = new System.Drawing.Size(175, 86);
+            this.läggTillPatient.TabIndex = 0;
+            this.läggTillPatient.Text = "Lägg Till Patient";
+            this.läggTillPatient.UseVisualStyleBackColor = true;
+            // 
+            // ändraBokning
+            // 
+            this.ändraBokning.Location = new System.Drawing.Point(515, 68);
+            this.ändraBokning.Name = "ändraBokning";
+            this.ändraBokning.Size = new System.Drawing.Size(162, 86);
+            this.ändraBokning.TabIndex = 1;
+            this.ändraBokning.Text = "Ändra Bokning";
+            this.ändraBokning.UseVisualStyleBackColor = true;
+            // 
+            // Receptionist
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1113, 471);
+            this.Controls.Add(this.ändraBokning);
+            this.Controls.Add(this.läggTillPatient);
+            this.MaximumSize = new System.Drawing.Size(1131, 518);
+            this.MinimumSize = new System.Drawing.Size(1131, 518);
+            this.Name = "Receptionist";
             this.Text = "Receptionist";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button läggTillPatient;
+        private System.Windows.Forms.Button ändraBokning;
     }
 }
