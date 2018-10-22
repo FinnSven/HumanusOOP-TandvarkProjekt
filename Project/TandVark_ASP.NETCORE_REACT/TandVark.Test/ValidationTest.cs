4aba856386edd8bad63f0761b993400ed7b68d2b
StankovicMarko89 <43344688+StankovicMarko89@users.noreply.github.com>
2018-10-22 13:32:02 +0200

    Skapat en controller och en valideringstest (ej klar)

---
+++ Project/TandVark_ASP.NETCORE_REACT/TandVark.Test/ValidationTest.cs
@@ -0,0 +1,15 @@
+﻿using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TandVark.Domain.Models;
+using TandVark.Domain.Repositories.Interfaces;
+using Xunit;
+
+namespace TandVark.UnitTest
+{
+    class ValidationTest
+    {
+
+    }
+}
